21e9fc2c5692df1810aacdbd194dcc26494286e3
LuisComS <dev@luiscoms.com.br>
2014-11-11 13:28:55 -0200

    Added initial application

---
+++ README.md
@@ -0,0 +1,20 @@
+Ask to Speaker
+==================
+
+Simple application to post questions
+
+First, download dependencies
+
+```bash
+npm install
+```
+
+Then, start server
+
+```bash
+npm start
+```
+
+Now open in localhost
+
+http://localhost:7777/
---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,16 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+        /* listitem  BrazilJS */
+    
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
